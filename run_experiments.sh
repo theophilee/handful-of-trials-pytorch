8c5839073e3511e75aca32f38496b125957dacb2
theophilee <theophile.gervet@gmail.com>
2019-05-28 15:03:37 -0400

    argparse bug

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,12 +2,11 @@
 
 trap "kill 0" EXIT
 
-#CUDA_VISIBLE_DEVICES=1 python main.py --env half_cheetah --activation tanh &
-#CUDA_VISIBLE_DEVICES=2 python main.py --env half_cheetah --activation swish &
+CUDA_VISIBLE_DEVICES=2 python main.py --env half_cheetah --hid_features 200,200
 
-python cem_comparison.py MySwimmer-v2 gaussian -l 30 -p 1000 -i 5
-python cem_comparison.py MySwimmer-v2 gaussian -l 20 -p 1000 -i 5
-python cem_comparison.py MySwimmer-v2 gaussian -l 30 -p 1000 -i 10
-python cem_comparison.py MySwimmer-v2 gaussian -l 20 -p 1000 -i 10
+#python cem_comparison.py MySwimmer-v2 gaussian -l 30 -p 1000 -i 5
+#python cem_comparison.py MySwimmer-v2 gaussian -l 20 -p 1000 -i 5
+#python cem_comparison.py MySwimmer-v2 gaussian -l 30 -p 1000 -i 10
+#python cem_comparison.py MySwimmer-v2 gaussian -l 20 -p 1000 -i 10
 
 wait

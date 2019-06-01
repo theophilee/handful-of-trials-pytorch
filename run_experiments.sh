52f1ddb530db275e8b07b807d157878150d9e4c8
theophilee <theophile.gervet@gmail.com>
2019-06-01 12:10:22 -0400

    cleanup

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,11 +2,8 @@
 
 trap "kill 0" EXIT
 
-CUDA_VISIBLE_DEVICES=2 python main.py --env half_cheetah --hid_features 200,200
-
-#python cem_comparison.py MySwimmer-v2 gaussian -l 30 -p 1000 -i 5
-#python cem_comparison.py MySwimmer-v2 gaussian -l 20 -p 1000 -i 5
-#python cem_comparison.py MySwimmer-v2 gaussian -l 30 -p 1000 -i 10
-#python cem_comparison.py MySwimmer-v2 gaussian -l 20 -p 1000 -i 10
+CUDA_VISIBLE_DEVICES=1 python main.py --env swimmer &
+CUDA_VISIBLE_DEVICES=2 python main.py --env pusher &
+CUDA_VISIBLE_DEVICES=2 python main.py --env cartpole &
 
 wait

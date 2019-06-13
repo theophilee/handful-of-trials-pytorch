55be848c93932d1b65176f03d4edd23fb796fe32
theophilee <theophile.gervet@gmail.com>
2019-06-13 14:50:20 -0400

    update

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,7 +2,7 @@
 
 trap "kill 0" EXIT
 
-CUDA_VISIBLE_DEVICES=0 python main.py --activation swish &
+CUDA_VISIBLE_DEVICES=1 python main.py --activation swish &
 CUDA_VISIBLE_DEVICES=2 python main.py --activation relu &
 
 #python cem_gym.py MyWalker2d-v2 -r 2 -l 25 -i 5 -e 5 --save
